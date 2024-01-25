ed06ded2a90bba873481b19c5a9842a616d237bb
wwwchendev <wwwchendev@gmail.com>
2024-01-25 23:52:29 +0800

    feat✨: 完成按分類顯示商品區塊

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
-
+import CategoryShowCase from './CategoryShowCase'
 
 const Home = () => {
   return (
     <>
       <Banner />
       <HomeCategory />
+      <CategoryShowCase />
     </>
   )
 }
