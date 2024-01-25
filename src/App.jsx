7afc304b1da78417073f5b22415ce324de076e24
wwwchendev <wwwchendev@gmail.com>
2024-01-25 17:32:51 +0800

    feat✨: 完成響應式Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import NavItems from './components/NavItems'
 
 function App() {
 
   return (
     <>
-      <nav>nav bar</nav>
+      <NavItems />
       <Outlet />
     </>
   )
