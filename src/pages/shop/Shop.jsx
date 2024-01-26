fe360d9cd1ff0199d303858c5e665807e4e2d6cb
wwwchendev <wwwchendev@gmail.com>
2024-01-26 18:44:26 +0800

    feat✨: 完成商店頁面版型

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../../components/PageHeader'
+import Data from '../../products.json'
+const showResults = '顯示 01 - 12 共 139 項商品'
 
 const Shop = () => {
+  const [GridList, setGridList] = useState(true)
+  const [products, setProducts] = useState(Data)
   return (
     <>
-      <PageHeader title='Our Shop Page' currentPage='Shop' />
-      <div className=''>Shop</div>
+      <PageHeader title='線上購物' currentPage='Shop' />
+      {/* Shop Page */}
+      <div className='shop-page padding-tb'>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                {/* layout and title here  */}
+                <div className="shop-title d-flex flex-wrap justify-content-between">
+                  <p>{showResults}</p>
+                  <div className={`product-view-mode ${GridList ? 'gridActive' : 'listActive'}`}>
+                    <a className='grid' onClick={() => { setGridList(!GridList) }}>
+                      <i className="icofont-ghost"></i>
+                    </a>
+                    <a className='list' onClick={() => { setGridList(!GridList) }}>
+                      <i className="icofont-listine-dots"></i>
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">
+              right-here
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
